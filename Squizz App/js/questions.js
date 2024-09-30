47208770adbc308b95513d1e528ae1924e789276
M'lakiy Andros <168479740+KurtisNfa@users.noreply.github.com>
2024-09-30 11:14:14 +0000

    Add files via upload

---
+++ Squizz App/js/questions.js
@@ -0,0 +1,122 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    answer2: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    answer2: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "When did LaJoconde get painted??",
+    answer: "between 1503-1506",
+    answer2: "between 1503-1506",
+    options: [
+      "1492",
+      "1501",
+      "between 1503-1506",
+      "who cares?...She's already painted"
+    ]
+  },
+    {
+    numb: 4,
+    question: "For what work is Karl Marx known",
+    answer: "Capital",
+    answer2: "Capital",
+    options: [
+      "Capital",
+      "Persian letters",
+      "The Republic",
+      "Faust"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does JS stand for?",
+    answer: "JavaScript",
+    answer2: "JavaScript",
+    options: [
+      "JavaScript",
+      "Joyfull Sins",
+      "means nothing",
+      "none above"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Blaise Pascal is known for being...",
+    answer: "a philosopher",
+    answer2: "a philosopher",
+    options: [
+      "a mathematician",
+      "a philosopher",
+      "Blaise who?...Pascal what?",
+      "a gate-keeper"
+    ]
+  },
+  {
+    numb: 7,
+    question: "who wrote the book of first Chronicle (in the Bible)?",
+    answer: "be humble...say you don't know",
+    answer2: "be humble...say you don't know",
+    options: [
+      "Esdras the Scribe",
+      "Rabbi Aquiva",
+      "the Catholic Church",
+      "be humble...say you don't know"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which is the longest surah? (with how many verses?) &#128064 &#128064 &#128064",
+    answer: "none above",
+    answer2: "none above",
+    options: [
+      "Al - Baqara (200 verses)",
+      "Al - Fatiha (7 verses)",
+      "Al - Kawthar (3 verses)",
+      "none above"
+    ]
+  },
+  {
+    numb: 9,
+    question: "can you translate 'Spill the beans' in french?",
+    answer: "Vendre la mèche",
+    answer2: "Vendre la mèche",
+    options: [
+      "Renverser les haricots",
+      "manquer d'attention",
+      "Vendre la mèche",
+      "Tourner autour du pot",
+    ]
+  },
+  {
+    numb: 10,
+    question: "how to delete a folder in the terminal?",
+    answer: "rm -r folder_name",
+    answer2: "rm -rf folder_name",
+    options: [
+      "rm -r folder_name",
+      "rm -rf folder_name",
+      "delete folder_name",
+      "cannot do it (need to be manual)"
+    ]
+  },
+];
